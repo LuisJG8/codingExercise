636891f585ef24b32c904bb3b23b9eff7c7718ec
Luis Gonzalez <162064187+LuisJG8@users.noreply.github.com>
2024-07-13 10:53:07 -0400

    Update codingExercise.py

--- codingExercise.py
+++ codingExercise.py
@@ -36,17 +36,16 @@ print(days)
 #   else:
 #     return False
 
-# # TODO: Add more code here 👇
+#
 # def days_in_month(year, month):
 #   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
 #   if month == 2 and is_leap(year):
 #     return 29
 #   else:
 #     return month_days[month - 1]
-
-# #🚨 Do NOT change any of the code below 
-# year = int(input()) # Enter a year
-# month = int(input()) # Enter a month
+# 
+# year = int(input()) 
+# month = int(input()) 
 # days = days_in_month(year, month)
 # print(days)
 
